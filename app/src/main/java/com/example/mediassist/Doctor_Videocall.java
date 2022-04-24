c10e7973a41ff66996ad2657c7261a8a041d4cb3
harsh <teammediassist@gmail.com>
2022-04-24 10:49:40 +0530

    jitsi , April 24

--- app/src/main/java/com/example/mediassist/Doctor_Videocall.java
+++ app/src/main/java/com/example/mediassist/Doctor_Videocall.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Doctor_Videocall extends AppCompatActivity {
@@ -36,7 +42,7 @@ public class Doctor_Videocall extends AppCompatActivity {
         secretCodeBox.setText(code);
 
         URL serverURL;
-/*
+
         try {
             serverURL = new URL("https://meet.jit.si");
             JitsiMeetConferenceOptions defaultOptions =
@@ -62,7 +68,7 @@ public class Doctor_Videocall extends AppCompatActivity {
                 JitsiMeetActivity.launch( Doctor_Videocall.this , options  );
             }
         });
-        */
+
 
 
     }
